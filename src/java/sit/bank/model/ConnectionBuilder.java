c8a4eeff813f79f6fc5d2ffa92cc924d091d4409
Chakree <chakree30584@gmail.com>
2014-12-10 13:57:41 +0700

    ConnectionBuilder

--- src/java/sit/bank/model/ConnectionBuilder.java
+++ src/java/sit/bank/model/ConnectionBuilder.java
@@ -4,10 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *
- * @author chakree30584
- */
 public class ConnectionBuilder {
     public static Connection getConnection() {
         String url = "jdbc:mysql://server.chakree.me:3306/sitbank";
