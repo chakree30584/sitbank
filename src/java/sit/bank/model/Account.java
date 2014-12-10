8e1c10ad09395363b47a5fd381deaba1b0236c8d
AON <AON@GDRAAON>
2014-12-11 00:40:14 +0700

    Aon commitแล้วนะ

--- src/java/sit/bank/model/Account.java
+++ src/java/sit/bank/model/Account.java
@@ -6,6 +6,8 @@
 
 package sit.bank.model;
 
+import java.util.Date;
+
 /**
  *
  * @author Man
@@ -16,6 +18,64 @@ public class Account {
     private String type;
     private Double balance;
     private int userId;
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    
+        
+    
+    public void deposit(double amount){
+        if (amount > 0) {
+            setBalance(this.balance + amount);
+        } else {
+            System.out.println("Deposit: 0");
+        }
+    }
+    
+    public void withdraw(double amount){
+        if (this.balance - amount < 0) {
+            System.out.println("Not enough balance (balance=" + balance + "), amount=" + amount + ")");
+        } else if (amount > 0) {
+            setBalance(this.balance - amount);
+        }
+    }
     
     
 }
